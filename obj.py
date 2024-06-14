0e27806a406894260da284a8f7fa5bfad030fb54
m-suj <mateusz.sujewicz1234567@gmail.com>
2024-06-14 18:19:17 +0200

    Rewrote Object Ray Casting to better fit the symbolic representation of image reflection

--- obj.py
+++ obj.py
@@ -1,10 +1,6 @@
 import arcade
 from math import radians, tan, sqrt
 
-MIRROR_X = 300
-MIRROR_Y = 300
-MIRROR_R = 200
-
 
 def print_prec(args: dict[str: float]):
     for k, v in args.items():
@@ -13,106 +9,98 @@ def print_prec(args: dict[str: float]):
     print()
 
 
-class Ray:
-    def __init__(self, angle, delay=0, color=arcade.color.BRICK_RED):
-        self.angle = angle
-        self.color = color
-        theta = radians(angle)
-        a = tan(theta)
-        b = MIRROR_Y - a * OBJECT_POSX
-        start_x, start_y = OBJECT_POSX, MIRROR_Y + delay
-        x_o, y_o = MIRROR_X, MIRROR_Y
-        eq_a, eq_b, eq_c = (
-            1 + a**2,
-            -2*x_o + 2*a*(b-y_o),
-            x_o ** 2 + (b - y_o) ** 2 - MIRROR_R ** 2
-        )
-        delta = eq_b ** 2 - 4 * eq_a * eq_c
-        d = sqrt(delta)
-        n, d = -eq_b + d, 2 * eq_a
-        end_x = n / d
-        end_y = a * end_x + b
-
-        # Reflected ray
-        # Tangent
-        a_t = (x_o - end_x) / (end_y - y_o)
-        b_t = end_y - a_t * end_x
-
-        # Perpendicular
-        a_p = -1 / a_t
-        b_p = end_y - a_p * end_x
-
-        # Parallel
-        b_s = start_y - a_t * start_x
-
-        # Cross point
-        x_x = (b_s - b_p) / (a_p - a_t)
-        y_x = a_t * x_x + b_s
-
-        # Symmetrical point
-        delta_x = x_x - start_x
-        delta_y = y_x - start_y
-        x_s, y_s = x_x + delta_x, y_x + delta_y
-
-        # Reflected ray
-        a_r = (end_y - y_s) / (end_x - x_s)
-        b_r = y_s - a_r * x_s
-
-
-        self.start_x, self.start_y = start_x, start_y
-        self.end_x, self.end_y = end_x, end_y
-        self.a_t, self.b_t = a_t, b_t
-        self.a_r, self.b_r = a_r, b_r
-
-    def draw(self):
-        arcade.draw_line(
-            start_x=self.start_x,
-            start_y=self.start_y,
-            end_x=self.end_x,
-            end_y=self.end_y,
-            color=arcade.color.BLUE,
-            line_width=1
-        )
-        s, e = (self.end_x, self.end_y), (100, self.a_r*100 + self.b_r)
-        if self.a_r < 0 and self.end_y > MIRROR_Y or self.a_r > 0 and self.end_y < MIRROR_Y:
-            e = (600, self.a_r*600 + self.b_r)
-        arcade.draw_line(s[0], s[1], e[0], e[1], self.color, 1)
-
-
-class Object:
-    def __init__(self, x, height=50, color=arcade.color.BLUE):
-        self.x = x
-        self.height = height
-        self.color = color
-
-    def draw_arrow(self):
-        arcade.draw_line(self.x)
-
-    def draw(self):
-        self.draw_arrow()
-
-
 
 class Mirror:
-    def __init__(self, x, y, r, degrees):
-        self.x = x
-        self.y = y
+    def __init__(self, x_o: float, y_o: float, r: float, degrees):
+        self.x = x_o
+        self.y = y_o
         self.r = r
+        self.f = self.r / 2
         self.degrees = degrees
+        self.x_f, self.y_f = self.x + self.r / 2, self.y
 
     def draw(self):
         arcade.draw_line(0, self.y, 1200, self.y, arcade.color.GRAY)
         arcade.draw_arc_outline(self.x, self.y, self.r * 2, self.r * 2, arcade.color.BLACK, -self.degrees, self.degrees, 5)
-        arcade.draw_point(self.x, self.y, arcade.color.RED, 3)
+        arcade.draw_point(self.x, self.y, arcade.color.BLACK, 5)
         arcade.draw_point(self.x + self.r / 2, self.y, arcade.color.BLACK, 3)
+
+
+
+class Object:
+    def __init__(self, mirror: Mirror, x: float, height=50, color=arcade.color.BLUE):
+        self.mirror = mirror
+        self.x = x
+        self.height = height
+        self.color = color
+        x_o, y_o, r = self.mirror.x, self.mirror.y, self.mirror.r
+        b = y_o + self.height
+        delta = (-2*x_o)**2 - 4*(x_o**2 + b**2 + y_o**2 - 2*b*y_o - r**2)
+        # Tip of an arrow
+        self.x_s, self.y_s = x_o + r - self.x, y_o + self.height
+        # Cross Point of first ray
+        self.x_e = (2*x_o + sqrt(delta)) / 2
+        self.y_e = b
+        # Reflected first ray
+        x_f, y_f = self.mirror.x_f, self.mirror.y_f
+        self.a_r1 = (self.y_e - y_f) / (self.x_e - x_f)
+        self.b_r1 = y_f - self.a_r1 * x_f
+        # Cross Point of second ray
+        self.x_e2, self.y_e2 = x_o + r, y_o
+        # Reflected second ray
+        self.a_r2 = -(self.y_e2 - self.y_s) / (self.x_e2 - self.x_s)
+        self.b_r2 = self.y_e2 - self.a_r2 * self.x_e2
+
+        # Cross Point of two reflected rays
+        self.x_image = (self.b_r2 - self.b_r1) / (self.a_r1 - self.a_r2)
+        self.y_image = self.a_r2 * self.x_image + self.b_r2
+
+    def draw_arrow(self):
+        arcade.draw_line(self.x_s, self.y_s - self.height, self.x_s, self.y_s, self.color, 5)
+
+    def draw_arrow_image(self):
+        arcade.draw_line(self.x_image, self.y_s - self.height, self.x_image, self.y_image, self.color, 5)
+
+    def draw_first_ray(self):
+        arcade.draw_line(self.x_s, self.y_s, self.x_e, self.y_e, self.color, 3)
+        arcade.draw_line(self.x_e, self.y_e, 100, self.a_r1 * 100 + self.b_r1, self.color, 3)
+        # x < f
+        if self.x < self.mirror.f:
+            arcade.draw_line(self.x_e, self.y_e, 800, self.a_r1 * 800 + self.b_r1, self.color)
+
+    def draw_second_ray(self):
+        arcade.draw_line(self.x_s, self.y_s, self.x_e2, self.y_e2, self.color, 3)
+        arcade.draw_line(self.x_e2, self.y_e2, 100, self.a_r2 * 100 + self.b_r2, self.color, 3)
+        # x < f
+        if self.x < self.mirror.f:
+            arcade.draw_line(self.x_e2, self.y_e2, 800, self.a_r2 * 800 + self.b_r2, self.color)
+
+
+    def draw(self):
+        self.draw_arrow()
+        self.draw_first_ray()
+        self.draw_second_ray()
+        self.draw_arrow_image()
+
 
 
 class Simulation:
     def __init__(self):
-        self.mirror: Mirror = Mirror(MIRROR_X, MIRROR_Y, MIRROR_R, 90)
-        self.objects: list[Object] = [Object(100)]
+        self.mirror: Mirror = Mirror(
+            x_o=300,
+            y_o=300,
+            r=200,
+            degrees=80,
+        )
+        self.objects: list[Object] = [
+            Object(self.mirror, x=300, height=50, color=arcade.color.RED),
+            Object(self.mirror, x=200, height=50, color=arcade.color.ORANGE),
+            Object(self.mirror, x=150, height=50, color=arcade.color.YELLOW_GREEN),
+            Object(self.mirror, x=100, height=50, color=arcade.color.BLUE),
+            Object(self.mirror, x=50, height=50, color=arcade.color.PURPLE),
+        ]
 
     def draw(self):
         for obj in self.objects:
             obj.draw()
-        arcade.draw_point(OBJECT_POSX, self.mirror.y, arcade.color.BUD_GREEN, 6)
+        self.mirror.draw()
